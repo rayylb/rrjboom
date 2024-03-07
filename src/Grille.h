c790eedd66462c88774b6cbf44222a3e173cad2d
Rym Zerzouh <rymzerzouh@gmail.com>
2024-03-07 15:21:51 +0100

    typo

--- src/Grille.h
+++ src/Grille.h
@@ -3,60 +3,27 @@
 
 #include "Bloc.h"
 
-const int GRILLE_DIMX = 13;
-const int GRILLE_DIMY = 7;
-
-/**
- * @class Grille
- * 
- * @brief La classe Grille comporte l'ensemble des blocs qui forment le terrain de jeu. Elle ne comporte pas les joueurs ou les bombes.
-*/
+/// constantes GRILLE_DIMX et GRILLE_DIMY
+const int GRILLE_DIMX = 7;
+const int GRILLE_DIMY = 5;
 
 class Grille {
-    private:
-        Bloc terrain[GRILLE_DIMX][GRILLE_DIMY];
+private:
+    Bloc terrain[GRILLE_DIMX][GRILLE_DIMY];
 
-    public:
-        /**
-         * @brief Récupère la dimension de la grille.
-         * 
-         * @param x Largeur de la grille.
-         * @param y Hauteur de la grille.
-         */
-        void getDim(int x, int y);
+public:
+    Grille();
+    ~Grille();
 
-        /**
-         * @brief Génère un terrain aléatoirement, prêt à être joué dessus.
-        */
-        void initGrille();
-        /**
-         * @brief Affiche la grille dans le terminal.
-        */
-        void affichTerminal();
+    void getDim(int x, int y);
+    void initGrille();
+    void affichTerminal();
 
-        /**
-         * @brief Récupère une case de la grille sans la modifier.
-         * 
-         * @param x Abscisse du bloc à récupérer.
-         * @param y Ordonnée du bloc à récupérer.
-         * @param copie Copie du bloc demandé.
-        */
-        void infoCase(int x, int y, Bloc& copie);
+    void infoCase(int x, int y, Bloc& copie);
+    Bloc& infoCase(int x, int y);
 
-        /**
-         * @brief Détruit le bloc sur une case de la grille, si elle est destructible.
-         * 
-         * @param x Abscisse du bloc qu'on essaie de détruire.
-         * @param y Ordonnée du bloc qu'on essaie de détruire
-        */
-        void detruireCase(int x, int  y);
-        /**
-         * @brief Récupère un bonus sur une case de la grille, si elle en comporte un.
-         * 
-         * @param x Abscisse du bonus qu'on essaie de récupérer.
-         * @param y Ordonnée du bonus qu'on essaie de récupérer.
-        */
-        void recupererBonus(int x, int y);
+    void detruireCase(int x, int  y);
+    void recupererBonus(int x, int y);
 };
 
 #endif
