300b8bbceaf8e8a0ea9e6b14b9ebaaf5d6943a51
P2201104 <rayane.labzizi@etu.univ-lyon1.fr>
2024-03-12 10:28:16 +0100

    Doc Explosion.h

--- src/Core/Explosion.h
+++ src/Core/Explosion.h
@@ -1,20 +1,44 @@
 #ifndef _EXPLOSION_H
 #define _EXPLOSION_H
 
+/**
+ * 
+ * @class Explosion
+ * @brief Classe représentant une explosion.
+ * 
+ * L'explosion est créée par une bombe après son explosion. Elle détruit les murs
+ * et tue les joueurs à proximité.
+ */
 class Explosion {
     private:
-        int x;
-        int y;
-        int timer;
+        int x; ///<  Indique la position horizontale de l'explosion
+        int y; ///<  Indique la position verticale de l'explosion
+        int timer; ///<  Indique le temps restant avant la fin de l'explosion
 
     public:
         Explosion() : x(0), y(0), timer(3) {};
         Explosion(int xpos, int ypos);
 
+        /*
+        * @brief Fonction qui retourne la position horizontale de l'explosion
+        * @return L'entier explosion.x qui est la position horizontale de l'explosion
+        */
         int getPosX();
+
+        /*
+        * @brief Fonction qui retourne la position verticale de l'explosion
+        * @return L'entier explosion.y qui est la position verticale de l'explosion
+        */
         int getPosY();
 
+        /*
+        * @brief Fonction qui avance d'une unité de temps, jusqu'à la fin de l'explosion
+        */
         void avancerTemps();
+
+        /*
+        * @brief Fonction qui retourne un booléen indiquant si l'explosion est terminée
+        */
         bool estActif();
 };
 
