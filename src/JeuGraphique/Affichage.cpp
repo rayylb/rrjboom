a31c2d42b4b87a98de2ebd1cbd03160d2d8256d1
Rymouu <151671598+Rymouu@users.noreply.github.com>
2024-03-12 10:54:40 +0100

    Update Affichage.cpp

--- src/JeuGraphique/Affichage.cpp
+++ src/JeuGraphique/Affichage.cpp
@@ -13,6 +13,7 @@ void Affichage::initFenetre(){
     SDL_CreateWindow("Rrjboom", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
      DimX, DimY, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
      rendu = SDL_CreateRenderer(fenetre,-1,SDL_RENDERER_ACCELERATED);
+    
      SDL_Surface *surfaceFond=IMG_Load("data/il faut choisir un fond.png");
      textureFond = SDL_CreateTextureFromSurface(rendu,surfaceFond);
 }
@@ -22,4 +23,8 @@ void Affichage::detruireFenetre(){
     SDL_DestroyRenderer(rendu);
     SDL_DestroyWindow(fenetre);
     SDL_Quit();
+}
+
+    SDL_Renderer* Affichage::getRenderer() {
+    return rendu;
 }
