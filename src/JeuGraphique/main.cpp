e54de3bbc1c4063399f99aed946717f4871393d4
Rymouu <rymzerzouh@gmail.com>
2024-03-14 11:35:43 +0100

    .

--- src/JeuGraphique/main.cpp
+++ src/JeuGraphique/main.cpp
@@ -9,6 +9,15 @@ int main(int argc,char *argv[]) {
   aff.initFenetre(800, 600);
   aff.afficherFenetre();
   //SDL_Delay(9000); 
+  bool running = true;
+  while(running){
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){
+      if(event.type == SDL_QUIT){
+        running = false;
+      }
+    }
+  }
   aff.detruireFenetre();
   return 0;
 }
