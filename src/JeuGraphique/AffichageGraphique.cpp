33970b63b4bea199c4177070714ecb718df90201
p2208504 <p2208504@B710L0114.univ-lyon1.fr>
2024-03-19 08:18:57 +0100

    classe jeugraphique

--- src/JeuGraphique/AffichageGraphique.cpp
+++ src/JeuGraphique/AffichageGraphique.cpp
@@ -14,10 +14,7 @@ void AffichageGraphique::initFenetre(int width, int height) {
 }
 
 void AffichageGraphique::afficherFenetre() {
-    SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
     SDL_RenderClear(rendu);
-    texture = IMG_LoadTexture(rendu, "bloc destructible.png");
-    SDL_RenderCopy(rendu, texture, NULL, NULL);
     SDL_RenderPresent(rendu);
 }
 
@@ -26,4 +23,16 @@ void AffichageGraphique::detruireFenetre() {
     SDL_DestroyRenderer(rendu);
     SDL_DestroyWindow(fenetre);
     SDL_Quit();
+}
+
+void AffichageGraphique::afficherRectangle(int posX, int posY) {
+
+}
+
+void AffichageGraphique::afficherSprite(int posX, int posY) {
+    SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
+    SDL_RenderClear(rendu);
+    texture = IMG_LoadTexture(rendu, "bloc destructible.png");
+    SDL_RenderCopy(rendu, texture, NULL, NULL);
+    SDL_RenderPresent(rendu);
 }
---
+++ src/JeuGraphique/JeuGraphique.h
@@ -0,0 +1,16 @@
+#ifndef _JEUGRAPHIQUE_H_
+#define _JEUGRAPHIQUE_H_
+
+#include <SDL2/SDL.h>
+#include "../Core/Partie.h"
+#include "AffichageGraphique.h"
+
+class JeuGraphique {
+    private:
+        Partie jeu;
+        AffichageGraphique affichage;
+
+    public:
+};
+
+#endif
