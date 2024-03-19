62cae57f3945c81fe22a597254f15c453ea94a45
p2208504 <p2208504@B710L0114.univ-lyon1.fr>
2024-03-19 10:18:01 +0100

    rectangle marche

--- src/JeuGraphique/JeuGraphique.cpp
+++ src/JeuGraphique/JeuGraphique.cpp
@@ -1,16 +1,40 @@
 #include "JeuGraphique.h"
+#include <iostream>
 
 void JeuGraphique::jeuRectangle() {
     affichage.initFenetre(800, 600);
     SDL_Event event;
     bool running = true;
+    int recX = 100;
+    int recY = 100;
+    int movX = 0;
+    int movY = 0;
     while(running) {
         while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT){
-                running = false;
-            }
+            switch(event.type) {
+                case SDL_QUIT : running = false; break;
+                case SDL_KEYDOWN :
+                    switch (event.key.keysym.sym) {
+                        case SDLK_UP : movY = -1; break;
+                        case SDLK_DOWN : movY = +1; break;
+                        case SDLK_LEFT : movX = -1; break;
+                        case SDLK_RIGHT : movX = +1; break;
+                    }
+                    break;
+                case SDL_KEYUP :
+                    switch (event.key.keysym.sym) {
+                        case SDLK_UP : if(movY<0) movY = 0; break;
+                        case SDLK_DOWN : if(movY>0) movY = 0; break;
+                        case SDLK_LEFT : if(movX<0) movX = 0; break;
+                        case SDLK_RIGHT : if(movX>0) movX = 0; break;
+                    }
+                    break;
+                }
         }
-        affichage.afficherRectangle(100, 100);
+        recX += (movX);
+        recY += (movY);
+        affichage.afficherRectangle(recX, recY);
+        SDL_Delay(16);
     }
     affichage.detruireFenetre();
 }
