6bd4cc76f671d83c6a9ab7b98791fcc556bbc77c
P2201104 <rayane.labzizi@etu.univ-lyon1.fr>
2024-03-21 14:20:22 +0100

    je fixe mes pb de push

--- src/JeuGraphique/JeuGraphique.h
+++ src/JeuGraphique/JeuGraphique.h
@@ -9,12 +9,18 @@ class JeuGraphique {
     private:
         Partie jeu;
         AffichageGraphique affichage;
+        float joueur1X;
+        float joueur1Y;
+        float joueur2X;
+        float joueur2Y;
+        int joueur1movX;
+        int joueur1movY;
+        int joueur2movX;
+        int joueur2movY;
 
     public:
         void jeuRectangle();
         void jeuSprite();
-<<<<<<< HEAD
-=======
 
         void boucleJeuMain();
         void nouvellePartie(int taille_bloc);
@@ -22,7 +28,6 @@ class JeuGraphique {
         void tourDeJeu(bool& stillRunning);
 
         void convertirTouches(float& joueurX, float& joueurY, char& mov);
->>>>>>> 83d8ddea33519c5330d729c2e102b9cee8e4bc5b
 };
 
 #endif
