4b20eeb6f198fc49d1bcabba963b5083cb78df69
Rymouu <rymzerzouh@gmail.com>
2024-03-26 10:40:28 +0100

    documentation bonus

--- src/Core/Bonus.h
+++ src/Core/Bonus.h
@@ -1,11 +1,16 @@
 #ifndef _BONUS_H
 #define _BONUS_H
 
+/**
+ * @brief Définition des types de bonus disponibles.
+ * @brief Enumération des types de bonus.
+ */
 enum BonusType {
-    Rien,
-    Fire,
-    Bomb,
-    Speed
+    Rien, /**< Aucun bonus */
+    Fire, /**< Bonus permettant de je sais pas*/
+    Bomb,  /**< Bonus permettant d'augmenter la portée d'explosion de la bombe'*/
+    Speed /**< Bonus augmentant la vitesse du joueur */
 };
 
 #endif
+
