aeeb6b606424071ac817c6806ec0891ed954c6ca
P2201104 <rayane.labzizi@etu.univ-lyon1.fr>
2024-03-26 09:12:54 +0100

    Commentaires de la classe Timer

--- src/JeuGraphique/Timer.h
+++ src/JeuGraphique/Timer.h
@@ -2,21 +2,60 @@
 #define _TIMER_H
 #include <SDL2/SDL.h>
 
+/*
+* @class Timer
+* @brief Classe représentant un timer dans le jeu
+* Elle contient deux entiers qui représentent le temps de début du timer et le temps de début de pause, 
+* ainsi que deux booléens qui indiquent si le timer a commencé et s'il est en pause
+*/
+
 class Timer {
     private:
-        int debut_timer;
-        int debut_pause;
-        bool paused;
-        bool started;
+        int debut_timer; ///< Le temps de début du timer
+        int debut_pause; ///< Le temps de fin du timer
+        bool paused; ///< Indique si le timer est en pause
+        bool started; ///< Indique si le timer a commencé
     
     public:
+
         Timer() : debut_timer(0), debut_pause(0), paused(false), started(false) {};
+
+        /*
+        * @brief Fonction qui démarre le timer
+        */
         void start();
+
+        /*
+        * @brief Fonction qui arrête le timer, en le réinitialisant
+        */
         void stop();
+
+        /* 
+        * @brief Fonction qui met en pause le timer
+        */
         void pause();
+
+        /*
+        * @brief Fonction qui enlève la pause du timer
+        */
         void unpause();
+
+        /*
+        * @brief Fonction qui retourne le temps écoulé depuis le début du timer
+        * @return L'entier qui représente le temps écoulé depuis le début du timer
+        */
         int temps_timer();
+
+        /*
+        * @brief Fonction qui indique si le timer est démarré
+        * @return Le booléen qui indique si le timer est démarré
+        */
         bool isStarted();
+
+        /*
+        * @brief Fonction qui indique si le timer est en pause
+        * @return Le booléen qui indique si le timer est en pause
+        */
         bool isPaused();
 
 };
